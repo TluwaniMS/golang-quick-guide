ced91dd0310b0a10555d246f32f5362487a65010
tluwaniMS <tluwani2020@gmail.com>
2023-12-27 16:57:49 +0200

    adding-else-if-statement-example

--- IfStatements/else-if-statement-example.go
+++ IfStatements/else-if-statement-example.go
@@ -2,5 +2,13 @@ package main
 import ("fmt")
 
 func main()  {
-
+	number := 11
+	
+	if(number <= 5){
+		fmt.Println("We're below 5!")
+	}else if(number > 5 && number <= 10){
+		fmt.Println("We're above 5!")
+	}else{
+		fmt.Println("We're way beyond 5!")
+	}
 }
